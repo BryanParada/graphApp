14b10350cc5234726c0048428505c5742c8f8377
Bryan Parada <bryan.parada.cea@gmail.com>
2022-10-31 21:01:13 -0300

    V334 - Menu items

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component  } from '@angular/core';
+
+interface menuItem {
+  route: string;
+  text: string;
+}
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styles: [
+    `
+    li {
+      cursor: pointer;
+    }
+    `
   ]
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent  {
+ 
+  menu: menuItem[] = [
+    {route: '/graphs/bars', text: 'Bars'},
+    {route: '/graphs/double-bars', text: 'Double Bars'},
+    {route: '/graphs/doughnut', text: 'Doughnut'},
+    {route: '/graphs/doughnut-http', text: 'Doughnut Http'},
 
-  constructor() { }
+  ];
 
-  ngOnInit(): void {
-  }
+ 
 
 }
