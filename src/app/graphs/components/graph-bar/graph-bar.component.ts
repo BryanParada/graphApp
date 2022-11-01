ba16f670df59152663863035ee8375bae39ec8d2
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-01 12:14:29 -0300

    V337 - Double Bars

--- src/app/graphs/components/graph-bar/graph-bar.component.ts
+++ src/app/graphs/components/graph-bar/graph-bar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import DataLabelsPlugin from 'chartjs-plugin-datalabels';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-graph-bar',
@@ -6,11 +9,60 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class GraphBarComponent implements OnInit {
+export class GraphBarComponent {
 
-  constructor() { }
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
-  ngOnInit(): void {
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      x: {},
+      y: {
+        min: 10,
+      },
+    },
+    plugins: {
+      legend: {
+        display: true,
+      },
+      datalabels: {
+        anchor: 'end',
+        align: 'end',
+      },
+    },
+  };
+  public barChartType: ChartType = 'bar';
+  public barChartPlugins = [DataLabelsPlugin];
+ 
+  public barChartData: ChartData<'bar'> = {
+    labels: ['2016','2017','2018', '2019', '2020', '2021', '2022', '2023', '2024'],
+    datasets: [
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#3280FA', hoverBackgroundColor: '#32DBBE' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#2B9BD9', hoverBackgroundColor: '#32DBBE' },
+      { data: [12, 43, 4, 13, 55, 53, 21] , label: 'Series C', backgroundColor: '#3CDEF0', hoverBackgroundColor: '#32DBBE' },
+    ],
+  };
+ 
+  // events
+  public chartClicked({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: {}[];
+  }): void {
+    console.log(event, active);
   }
-
+ 
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: {}[];
+  }): void {
+    console.log(event, active);
+  }
+ 
 }
